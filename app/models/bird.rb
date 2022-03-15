546c937f986935addde4af7e4ab5378448eb5ca4
Felipa Mendez <felipalouise@gmail.com>
2022-03-15 08:52:45 -0500

    commit tree 4fa87a813e4e6248e827c0e09b8c562592e35e34

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,16 @@
 class Bird < ApplicationRecord
+    def create
+        bird = Bird.create(bird_params)
+        render json: bird, status: :created
+      end
+
+      private
+        # all methods below here are private
+
+        def bird_params
+            params.permit(:name, :species)
+        end
+
+end
+
 end
